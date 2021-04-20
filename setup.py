dc3d9c76178b185e4242e49646a1e1e16b2c1a0d
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-04-19 20:55:30 -0400

    Autopep8 formatting

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='tags',
-   version='1.0',
-   description='Protoype module for GCP tags',
-   author='Rosemary Wang',
-   author_email='915624+joatmon08@users.noreply.github.com',
-   packages=find_packages('.'),
+    name='tags',
+    version='1.0',
+    description='Protoype module for GCP tags',
+    author='Rosemary Wang',
+    author_email='915624+joatmon08@users.noreply.github.com',
+    packages=find_packages('.'),
 )
